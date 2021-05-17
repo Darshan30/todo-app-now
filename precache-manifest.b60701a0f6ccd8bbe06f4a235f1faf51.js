39c33a38a808271d0cb220619b8ddb9043da94b0
Darshan S <darshansampathkumar@gmail.com>
2021-05-17 15:26:27 +0530

    Updates

---
+++ precache-manifest.b60701a0f6ccd8bbe06f4a235f1faf51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004342e5d0ac60b9f213df8fd9dd1fc0",
+    "url": "/todo-app-now/index.html"
+  },
+  {
+    "revision": "b667f2b29e3b64d81d13",
+    "url": "/todo-app-now/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "922e6a18d1aacb9cad7b",
+    "url": "/todo-app-now/static/css/main.c4f31b5d.chunk.css"
+  },
+  {
+    "revision": "b667f2b29e3b64d81d13",
+    "url": "/todo-app-now/static/js/2.b9bf3b32.chunk.js"
+  },
+  {
+    "revision": "be3ae91867bd4c806afeb90c76d5e54f",
+    "url": "/todo-app-now/static/js/2.b9bf3b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "922e6a18d1aacb9cad7b",
+    "url": "/todo-app-now/static/js/main.d7f88373.chunk.js"
+  },
+  {
+    "revision": "b94aa35578b59755b86e",
+    "url": "/todo-app-now/static/js/runtime-main.6c443f9b.js"
+  }
+]);
